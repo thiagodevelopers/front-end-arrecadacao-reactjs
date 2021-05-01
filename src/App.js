b1925d742467d200b6ce05d11ad5f300ea04fab9
Thiago de Souza Santos <tssantos@tjrj.jus.br>
2021-05-01 14:29:10 -0300

    Inicital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import api from './api';
+import ReactFlexyTable from "react-flexy-table"
+import "react-flexy-table/dist/index.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    arquivos: [],
+  }
+
+  async componentDidMount(){
+    const response = await api.get('tipo/remessa');
+
+    this.setState({arquivos: response.data});
+  }
+
+  render() {
+
+    const {arquivos} = this.state;
+
+    return <ReactFlexyTable data={arquivos} filterable nonFilterCols={["id", "quantLinhas","valorTotal"]} />
+  }
 }
 
 export default App;
